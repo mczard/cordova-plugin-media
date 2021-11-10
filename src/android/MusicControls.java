a35b361d54161293aad087c6088d38d2b72999db
Mateusz Czardybon <mateusz.czardybon@dieboldnixdorf.com>
2021-11-10 22:27:22 +0100

    NullReference Exception

--- src/android/MusicControls.java
+++ src/android/MusicControls.java
@@ -77,17 +77,23 @@ public class MusicControls {
 	}
 
 	// Register pendingIntent for broacast
-	public void registerMediaButtonEvent(){
+	public void registerMediaButtonEvent() {
 
-		this.mediaSessionCompat.setMediaButtonReceiver(this.mediaButtonPendingIntent);
+		if (this.mediaSessionCompat != null) {
+			this.mediaSessionCompat.setMediaButtonReceiver(this.mediaButtonPendingIntent);
+		}
 
 		/*if (this.mediaButtonAccess && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2){
 		this.mAudioManager.registerMediaButtonEventReceiver(this.mediaButtonPendingIntent);
 		}*/
 	}
 
-	public void unregisterMediaButtonEvent(){
-		this.mediaSessionCompat.setMediaButtonReceiver(null);
+	public void unregisterMediaButtonEvent() {
+
+		if (this.mediaSessionCompat != null) {
+			this.mediaSessionCompat.setMediaButtonReceiver(null);
+		}
+
 		/*if (this.mediaButtonAccess && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2){
 		this.mAudioManager.unregisterMediaButtonEventReceiver(this.mediaButtonPendingIntent);
 		}*/
